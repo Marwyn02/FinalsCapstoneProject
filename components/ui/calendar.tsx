b449529f15a7ca5bb1fdf2cab70036656955944a
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-09-03 19:17:33 +0800

    Update the calendar component

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -2,10 +2,9 @@
 
 import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { Chevron, DayPicker } from "react-day-picker";
+import { DayPicker } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
@@ -19,56 +18,15 @@ function Calendar({
     <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
-      // classNames={{
-      //   months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
-      //   month: "space-y-4",
-      //   month_caption: "flex justify-center pt-1 relative items-center",
-      //   caption_label: "text-sm font-medium",
-      //   nav: "space-x-1 flex items-center",
-      //   nav_button: cn(
-      //     buttonVariants({ variant: "outline" }),
-      //     "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
-      //   ),
-      //   button_previous: "absolute left-1",
-      //   button_next: "absolute right-1",
-      //   month_grid: "w-full border-collapse space-y-1",
-      //   weekdays: "flex",
-      //   weekday:
-      //     "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
-      //   week: "flex w-full mt-2",
-      //   day: cn(
-      //     "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
-      //     props.mode === "range"
-      //       ? "[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
-      //       : "[&:has([aria-selected])]:rounded-md"
-      //   ),
-      //   day_button: cn(
-      //     buttonVariants({ variant: "ghost" }),
-      //     "h-8 w-8 p-0 font-normal aria-selected:opacity-100"
-      //   ),
-      //   range_start: "day-range-start",
-      //   range_end: "day-range-end",
-      //   selected:
-      //     "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-      //   today: "bg-accent text-accent-foreground",
-      //   outside:
-      //     "day-outside text-muted-foreground opacity-50  aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
-      //   disabled: "text-muted-foreground opacity-50",
-      //   range_middle:
-      //     "aria-selected:bg-accent aria-selected:text-accent-foreground",
-      //   hidden: "invisible",
-      //   ...classNames,
-      // }}
-
       classNames={{
         months:
-          "flex justify-center sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
+          "relative grid grid-cols-1 justify-center -mt-5 sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4 w-full",
-        month_caption: "flex justify-center mb-6 relative items-center",
+        month_caption: "flex justify-center mb-2 relative items-center",
         caption_label: "text-sm font-light uppercase tracking-wider",
-        nav: "space-x-1",
-        button_previous: "absolute left-16",
-        button_next: "absolute right-16",
+        nav: "space-x-1 flex justify-between items-center mx-5 translate-y-9 z-50",
+        button_previous: "hover:text-gray-400 duration-300",
+        button_next: "hover:text-gray-400 duration-300",
         month_grid: "grid justify-center w-full md:w-full border-collapse",
         weekdays: "flex mb-3",
         weekday:
@@ -80,23 +38,16 @@ function Calendar({
             ? "[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
             : "[&:has([aria-selected])]:rounded-md"
         ),
-        // day: "h-12 w-12 text-center text-sm p-0 relative [&:has([aria-selected].range_end)]:rounded-r-full [&:has([aria-selected].outside)]:bg-slate-100/50 [&:has([aria-selected])]:bg-slate-100 first:[&:has([aria-selected])]:rounded-l-full last:[&:has([aria-selected])]:rounded-r-full focus-within:relative focus-within:z-20 dark:[&:has([aria-selected].day-outside)]:bg-slate-800/50 dark:[&:has([aria-selected])]:bg-slate-800",
         day_button: cn("h-12 w-12 p-0 font-normal "),
         range_start: "range_start",
         range_end: "range_end",
         selected:
-          // "rounded-0 bg-slate-900 text-slate-50 hover:bg-red-900 hover:text-white focus:bg-slate-900 focus:text-slate-50",
           "bg-slate-900 text-slate-50 hover:bg-slate-300 hover:text-slate-900 focus:bg-slate-900 focus:text-slate-50 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-900 dark:hover:text-slate-900 dark:focus:bg-slate-50 dark:focus:text-slate-900 duration-300",
         today: "bg-slate-400 text-slate-900",
-        // "rounded-2xl bg-slate-100 text-slate-900 dark:bg-slate-800 dark:text-slate-50",
         outside:
-          // " rounded-2xl outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
           "day-outside rounded-2xl text-slate-500 opacity-50 aria-selected:bg-slate-100/50 aria-selected:text-slate-500 aria-selected:opacity-30 dark:text-slate-400 dark:aria-selected:bg-slate-800/50 dark:aria-selected:text-slate-400",
         disabled: "text-slate-500 opacity-20",
-        // "text-slate-500 opacity-50 dark:text-slate-400",
         range_middle:
-          // "aria-selected:bg-slate-100 aria-selected:text-slate-900",
-
           "aria-selected:bg-slate-100 aria-selected:text-slate-900 dark:aria-selected:bg-slate-800 dark:aria-selected:text-slate-50",
         hidden: "invisible",
         ...classNames,
@@ -106,7 +57,7 @@ function Calendar({
           if (props.orientation === "left") {
             return <ChevronLeft {...props} />;
           }
-          return <Chevron {...props} />;
+          return <ChevronRight {...props} />;
         },
       }}
       {...props}
@@ -116,3 +67,10 @@ function Calendar({
 Calendar.displayName = "Calendar";
 
 export { Calendar };
+
+// day: "h-12 w-12 text-center text-sm p-0 relative [&:has([aria-selected].range_end)]:rounded-r-full [&:has([aria-selected].outside)]:bg-slate-100/50 [&:has([aria-selected])]:bg-slate-100 first:[&:has([aria-selected])]:rounded-l-full last:[&:has([aria-selected])]:rounded-r-full focus-within:relative focus-within:z-20 dark:[&:has([aria-selected].day-outside)]:bg-slate-800/50 dark:[&:has([aria-selected])]:bg-slate-800",
+// "rounded-0 bg-slate-900 text-slate-50 hover:bg-red-900 hover:text-white focus:bg-slate-900 focus:text-slate-50",
+// "rounded-2xl bg-slate-100 text-slate-900 dark:bg-slate-800 dark:text-slate-50",
+// " rounded-2xl outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
+// "text-slate-500 opacity-50 dark:text-slate-400",
+// "aria-selected:bg-slate-100 aria-selected:text-slate-900",
