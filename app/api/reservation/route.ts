b059d8d20f3665b0b67eeb5617eaa79dc1ff3d7e
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-10-16 23:03:39 +0800

    Add reservation create and findOne API

---
+++ app/api/reservation/route.ts
@@ -0,0 +1,63 @@
+"use server";
+
+import prisma from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+// import { withAccelerate } from "@prisma/extension-accelerate";
+// const prisma = new PrismaClient().$extends(withAccelerate());
+
+export async function createReservation(values: any) {
+  const {
+    reservationId,
+    prefix,
+    firstName,
+    lastName,
+    email,
+    contactNumber,
+    modeOfPayment,
+    checkIn,
+    checkOut,
+    adult,
+    children,
+    payment,
+  } = values;
+
+  try {
+    if (values.firstName !== "") {
+      await prisma.reservation.create({
+        data: {
+          reservationId,
+          prefix,
+          firstName,
+          lastName,
+          email,
+          phoneNumber: contactNumber,
+          modeOfPayment,
+          checkIn,
+          checkOut,
+          adult,
+          children,
+          payment,
+        },
+      });
+    }
+
+    revalidatePath("/reservation-success");
+  } catch (error) {
+    console.error("Error in reservation creation: ", error);
+    return { success: false, message: error };
+  } finally {
+    redirect("/reservation-success");
+  }
+}
+
+export async function getReservation(id: string) {
+  const reservation = await prisma.reservation.findFirst({
+    where: {
+      reservationId: id,
+    },
+  });
+
+  return reservation;
+}
