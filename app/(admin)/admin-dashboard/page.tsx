28367b15e002eb61b8d590a3d98fc835d8781454
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-10-18 16:17:59 +0800

    data fetching in admin side

--- app/(admin)/admin-dashboard/page.tsx
+++ app/(admin)/admin-dashboard/page.tsx
@@ -1,12 +1,17 @@
+"use server";
+
+import { getAllReservation } from "@/app/api/reservation/route";
 import { AdminAnalytics, UpcomingSchedule } from "@/app/ui/admin/overview";
 
-export default function Page() {
+export default async function Page() {
+  const reservations = await getAllReservation();
+
   return (
     <main className="px-24 py-12 space-y-3">
       <h3 className="text-2xl font-medium">Overview</h3>
 
-      <AdminAnalytics />
-      <UpcomingSchedule />
+      <AdminAnalytics reservations={reservations} />
+      <UpcomingSchedule reservations={reservations} />
     </main>
   );
 }
