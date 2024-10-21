fce04b24feee4c6b42b334ca756f701c06fc629f
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-10-21 16:26:40 +0800

    Add memories and revews server page

---
+++ app/(review)/memories-and-reviews/page.tsx
@@ -0,0 +1,11 @@
+import MemoriesPage from "@/app/ui/memories-and-reviews/memories";
+import ReviewsPage from "@/app/ui/memories-and-reviews/reviews";
+
+export default function MemoriesAndReviews() {
+  return (
+    <main className="bg-[#fcf4e9]">
+      <MemoriesPage />
+      <ReviewsPage />
+    </main>
+  );
+}
