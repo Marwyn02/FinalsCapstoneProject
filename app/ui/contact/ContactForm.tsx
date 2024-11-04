0c1d7dfa664bca9d095efbad73657a1b709699d0
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-11-04 10:05:14 +0800

    contact page mobile view

--- app/ui/contact/ContactForm.tsx
+++ app/ui/contact/ContactForm.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-
-import { ParallaxBanner } from "react-scroll-parallax";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -20,47 +18,6 @@ import {
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
-export function ContactPage() {
-  return (
-    <div>
-      <ParallaxBanner
-        layers={[{ image: "/image/room-2-1.jpg", speed: -15 }]}
-        className="w-full h-[300px] object-cover brightness-75 contrast-125"
-      />
-
-      <section className="grid grid-cols-2 gap-x-5">
-        {/* Contact title */}
-        <div className="space-y-5 ml-28 py-10">
-          <div>
-            <h1 className="text-6xl font-semibold font-teko">Contact us</h1>
-            <p className="font-semibold">Get in Touch with Us</p>
-          </div>
-
-          <p>
-            We’re here to help! Whether you have a question about our services,
-            need assistance with a booking, or want to share feedback, feel free
-            to reach out. Simply fill out the form below and a member of our
-            team will get back to you as soon as possible. We aim to respond to
-            all inquiries within 24 hours. We value your thoughts and look
-            forward to assisting you!
-            <br />
-            <br />
-            We value your thoughts and look forward to assisting you!
-          </p>
-
-          <div>
-            <p className="font-medium font-teko">Our email address:</p>
-            <p className="font-semibold">crisantotransienthouse@gmail.com</p>
-          </div>
-        </div>
-
-        {/* Contact form */}
-        <ContactForm />
-      </section>
-    </div>
-  );
-}
-
 const formSchema = z.object({
   name: z.string().min(2, {
     message: "Name must be at least 2 characters.",
@@ -70,7 +27,7 @@ const formSchema = z.object({
   message: z.string(),
 });
 
-export function ContactForm() {
+const ContactForm = () => {
   const router = useRouter();
   const [isSubmit, setIsSubmit] = useState(false);
 
@@ -89,11 +46,12 @@ export function ContactForm() {
       console.log(error);
     }
   }
+
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-3 bg-white p-10 pr-28"
+        className="space-y-3 bg-white px-5 py-10 md:p-10 md:pr-28"
       >
         <FormField
           control={form.control}
@@ -162,4 +120,6 @@ export function ContactForm() {
       </form>
     </Form>
   );
-}
+};
+
+export default ContactForm;
---
+++ app/ui/contact/ContactPage.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React from "react";
+import { ParallaxBanner } from "react-scroll-parallax";
+
+import ContactForm from "./ContactForm";
+
+const ContactPage = () => {
+  return (
+    <>
+      <ParallaxBanner
+        layers={[{ image: "/image/room-2-1.jpg", speed: -15 }]}
+        className="w-full h-[300px] object-cover brightness-75 contrast-125"
+      />
+
+      <section className="grid grid-cols-1 lg:grid-cols-2 gap-x-5">
+        {/* Contact title */}
+        <div className="space-y-5 px-5 md:ml-28 py-10">
+          <div>
+            <h1 className="text-6xl font-semibold font-teko">Contact us</h1>
+            <p className="font-semibold">Get in Touch with Us</p>
+          </div>
+
+          <p>
+            We’re here to help! Whether you have a question about our services,
+            need assistance with a booking, or want to share feedback, feel free
+            to reach out. Simply fill out the form below and a member of our
+            team will get back to you as soon as possible. We aim to respond to
+            all inquiries within 24 hours. We value your thoughts and look
+            forward to assisting you!
+            <br />
+            <br />
+            We value your thoughts and look forward to assisting you!
+          </p>
+
+          <div>
+            <p className="font-medium font-teko">Our email address:</p>
+            <p className="font-semibold">crisantotransienthouse@gmail.com</p>
+          </div>
+        </div>
+
+        {/* Contact form */}
+        <ContactForm />
+      </section>
+    </>
+  );
+};
+
+export default ContactPage;
