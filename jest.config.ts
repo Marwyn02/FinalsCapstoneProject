66bd43a09ec0bf9d2454ab7af897569b733f62b0
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-11-09 11:54:16 +0800

    add files

---
+++ jest.config.ts
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+
+  // Module file extensions for importing
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
