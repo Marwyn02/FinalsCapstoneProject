c0830ca8c239f7810d161f38fae598e0a61cafc2
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2024-12-12 14:07:28 +0800

    refactor reply apis

--- features/reply/api/CreateReply.ts
+++ features/reply/api/CreateReply.ts
@@ -1,10 +1,21 @@
 "use server";
 
 import prisma from "@/lib/db";
-import { Reply } from "@/app/lib/types/types";
 import { revalidatePath } from "next/cache";
+// import { AuditLog } from "@/features/audit/api/AuditLog";
 
-export async function CreateReply(values: Reply) {
+type ReplyType = {
+  id?: number;
+  replyId: string;
+  message: string;
+  author: string;
+  reviewId: string;
+  adminId?: string;
+  updatedAt?: Date;
+  createdAt?: Date;
+};
+
+export async function CreateReply(values: ReplyType) {
   const { replyId, message, author, reviewId, adminId } = values;
 
   if (!message || !author || !reviewId || !adminId) {
@@ -21,6 +32,21 @@ export async function CreateReply(values: Reply) {
 
   // If this is true, then the process is in update state
   if (res) {
+    const values = {
+      adminId,
+      username: author,
+      resourceType: "Reply ID",
+      resourceId: res.replyId,
+      details: {
+        updateReply: {
+          from: `From: ${res.message}`,
+          to: `To: ${message}`,
+        },
+      },
+    };
+
+    // await AuditLog("Update Reply", values);
+
     await prisma.reply.update({
       where: {
         replyId: res.replyId,
@@ -45,6 +71,18 @@ export async function CreateReply(values: Reply) {
       addedBy: adminId,
     },
   });
+
+  // const values1 = {
+  //   adminId,
+  //   username: author,
+  //   resourceType: "Reply ID",
+  //   resourceId: replyId,
+  //   details: {
+  //     createReply: { reply: `Add: ${message}` },
+  //   },
+  // };
+
+  // await AuditLog("Create Reply", values1);
 
   revalidatePath("/admin-dashboard/reviews");
 }
--- features/reply/api/DeleteReply.ts
+++ features/reply/api/DeleteReply.ts
@@ -2,8 +2,13 @@
 
 import prisma from "@/lib/db";
 import { revalidatePath } from "next/cache";
+// import { AuditLog } from "@/features/audit/api/AuditLog";
 
-export async function DeleteReply(replyId: string, adminId: string) {
+export async function DeleteReply(
+  replyId: string,
+  adminId: string
+  // username: string
+) {
   if (replyId && adminId) {
     await prisma.reply.update({
       where: {
@@ -12,6 +17,15 @@ export async function DeleteReply(replyId: string, adminId: string) {
       data: { removedBy: adminId, isDeleted: true, deletedAt: new Date() },
     });
   }
+
+  // const values = {
+  //   username,
+  //   adminId,
+  //   resourceType: "Reply ID",
+  //   resourceId: replyId,
+  // };
+
+  // await AuditLog("Delete Reply", values);
 
   revalidatePath("/admin-dashboard/reviews");
 }
