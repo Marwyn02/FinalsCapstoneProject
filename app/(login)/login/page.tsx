14ff2174ec8a822fe07995658f2b0ba6e4becf09
Marwyn02 <jhunmarwynsumargo@yahoo.com>
2025-02-23 11:06:58 +0800

    update login page

--- app/(login)/login/page.tsx
+++ app/(login)/login/page.tsx
@@ -1,6 +1,8 @@
-import Login from "@/app/ui/auth/login";
+import Image from "next/image";
 import { getCurrentUser } from "@/lib/session";
 import { redirect } from "next/navigation";
+
+import Login from "@/features/auth/components/Login";
 
 const Page = async () => {
   const user = await getCurrentUser();
@@ -10,7 +12,13 @@ const Page = async () => {
   }
   return (
     <main className="grid grid-cols-6 bg-white h-screen">
-      <></>
+      <Image
+        src={"/image/LoginImage.jpg"}
+        alt="Crisanto"
+        height={1000}
+        width={1000}
+        className="w-full h-full col-span-4"
+      />
       <Login />
     </main>
   );
